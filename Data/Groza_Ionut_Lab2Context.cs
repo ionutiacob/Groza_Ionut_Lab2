428b5b7610227842a1f7dde490d734c957ff9df1
Ionut Groza <ionut.iacob.groza@gmail.com>
2023-10-22 16:08:39 +0300

    Add project files.

---
+++ Data/Groza_Ionut_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Groza_Ionut_Lab2.Models;
+
+namespace Groza_Ionut_Lab2.Data
+{
+    public class Groza_Ionut_Lab2Context : DbContext
+    {
+        public Groza_Ionut_Lab2Context (DbContextOptions<Groza_Ionut_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Groza_Ionut_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Groza_Ionut_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
